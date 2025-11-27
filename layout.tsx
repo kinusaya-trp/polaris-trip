fa037d133366fda7dc612222c9e8553eb960a0c8
kinusaya-trp <s24.w@i.softbank.jp>
2025-11-28 02:23:02 +0900

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,1 +1,7 @@
-export default function RootLayout({children}:{children:React.ReactNode}){return (<html><body>{children}</body></html>)}
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
